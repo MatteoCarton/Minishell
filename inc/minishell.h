6d309c29d1df159efec2984a4358d99befbfdcf6
Mattéo <mcarton@student.s19.be>
2025-05-28 15:10:01 +0200

    struct

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,13 +1,29 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <unistd.h>
-# include <stdio.h>
-# include <signal.h>
-# include <readline/readline.h>
 # include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 
+typedef enum s_token_type
+{
+	END_TOKEN,
+	SINGLE_QUOTE,
+	DOUBLE_QUOTE,
+	PIPE,
+	IN,
+	OUT,
+	DOLLAR,
+}					t_token_type;
 
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*str;
+	struct s_token	*next;
+}					t_token;
 
 #endif
