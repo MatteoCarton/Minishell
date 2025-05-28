dd7caa4569da0d36aef5e3b9c158481dee8670d1
feyta <parallels@linux.localdomain>
2025-05-28 16:28:36 +0200

    lexer debut

--- srcs/main.c
+++ srcs/main.c
@@ -1,21 +1,36 @@
 #include "../inc/minishell.h"
 
-int main(int argc, char **argv)
+int start(char *line)
+{
+    t_token *token;
+
+    token = get_token(line);
+    if (!token)
+    {
+        //exit
+    }
+    //parser
+
+}
+
+
+
+int main(int argc, char **argv, char **env)
 {
     (void)argc;
     (void)argv;
-    
     char *line;
-    
+
+
     while (19)
     {
         line = readline("matteoshell$ ");
         if (!line) // si on CTRL+D (car ca renvoie NULL)
             break ;
-        if (*line) // si elle est pas vide
-        {
-            add_history(line);
-        }
+        if (!line || check_line(line))
+            break;
+        add_history(line);
+        if (!start(line))
         free(line);
     }
     return (0);
